dc8783176d37f33197f51f986f5649178bf2b6bc
krupashni <krupashni22110281@snuchennai.edu.in>
2025-11-03 14:26:00 +0530

    Final

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Krupashni on Azure CI/CD Pipeline!');
+  res.send('Hello from Krupashni J on Azure CI/CD Pipeline!');
 });
 
 app.listen(port, () => console.log(`App running on port ${port}`));
